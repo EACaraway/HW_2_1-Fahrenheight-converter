b0b0eb854f1ac40f3052bfae91de400c71490e11
Evan Caraway <evan.caraway@outlook.com>
2020-11-24 23:48:46 -0800

    Project Files

---
+++ homeWorkWeek2Problem1 Fahrenheight converter/homeWorkWeek2Problem1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkWeek2Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                                                            // Evan Caraway, Sirnivas Reddy Doma, Kipack Jeong
+
+            Console.Write("Enter temperature in degrees Fahrenheit: ");     // ask user for temp.
+
+            string temp = Console.ReadLine();                               // read user input and save in temp.
+
+            double tempInFahrenheit = Convert.ToDouble(temp);               // convert temp to double.
+            double tempInCelsius = (5.0 / 9.0) * (tempInFahrenheit - 32);   // formula for converting from Fahrenheit to Celsius.
+            tempInCelsius = Math.Round(tempInCelsius, 2);                   // round to 2 decimal places.
+            Console.WriteLine($"The temperature is {tempInCelsius} degrees Celsius." );
+        }
+    }
+}
